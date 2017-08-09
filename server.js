b5d681e703f1aff201bce05be5b98139df022a1b
lightchoda <choda.maheshbabu123@gmail.com>
2017-08-09 10:51:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/friends',function (req,res){
-   res.send("My friends are not smarter as think, they even worse"); 
+   res.sendFile('friends.html'); 
 });
 
 app.get('/ui/madi.png', function (req, res) {
